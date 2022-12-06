ec436031dd49fbc45753690b84fc340d24edfb2f
Rathalin <danielflo1997@hotmail.com>
2022-12-06 09:42:29 +0100

    Setup vue-i18n

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,16 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue'
+import { i18n } from './locales/i18n'
+import router from './router'
 
-import "./assets/main.css";
+import './assets/main.css'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
+app.use(createPinia())
+app.use(i18n)
+app.use(router)
 
-app.mount("#app");
+app.mount('#app')
